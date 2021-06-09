8df1a954a03098f01b8f6c304c676b074dd7141a
Prathiksha Poojary <47886532+cprathiksha@users.noreply.github.com>
2021-06-09 18:15:15 +0530

    Update mypython.py

--- mypython.py
+++ mypython.py
@@ -3,11 +3,11 @@ import time
 
 sometext = "Hi there!"
 
-html_content = f"<html> <head> <h1>{sometext}</h1> </head> <body> </body> </hmtl>" //assigning html tags to variable
+html_content = f"<html> <head> <h1>{sometext}</h1> </head> <body> </body> </hmtl>" 
 
-with open("index.html","w") as html_file:  //creating index.html file with write file permission
+with open("index.html","w") as html_file:  
     html_file.write(html_content)  
     print("Html file created!")
 
 time.sleep(2);
-webbrowser.open_new_tab("index.html")  //to open html file in browser
+webbrowser.open_new_tab("index.html") 
